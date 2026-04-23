80a099b4f3ca617edeca130fab3e3b27df0f86e5
Anees <89216202+MuftiAnees@users.noreply.github.com>
2026-04-23 19:23:43 +0500

    style task screen layout, sidebar and input row

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 console.log("One Step is running");
 const button = document.querySelector(".btn-primary");
 const launchScreen = document.querySelector(".launch-screen");
+
 button.addEventListener("click", function () {
   launchScreen.style.display = "none";
+  document.querySelector(".task-screen").style.display = "flex";
 });
